21b5211b3afd0b6dd8211fceaf84d2f9dfca1679
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-07-30 10:02:53 +0300

    [DTO] Adding DTO for read/write

---
+++ PlatformService/Data/DTO/PlatformCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using OriApps.UniCommand.PlatformService.Models.Cost;
+
+namespace OriApps.UniCommand.PlatformService.Data.DTO;
+
+public class PlatformCreateDTO
+{
+	[Required]
+	public string Name { get; set; }
+	
+	[Required]
+	public string Publisher { get; set; }
+	
+	[Required]
+	public CostType Cost { get; set; }
+}
---
+++ PlatformService/Data/DTO/PlatformReadDTO.cs
@@ -0,0 +1,11 @@
+﻿using OriApps.UniCommand.PlatformService.Models.Cost;
+
+namespace OriApps.UniCommand.PlatformService.Data.DTO;
+
+public class PlatformReadDTO
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Publisher { get; set; }
+	public CostType Cost { get; set; }
+}
