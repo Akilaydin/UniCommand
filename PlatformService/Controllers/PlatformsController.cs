bd5d6ef5c6cff1195119d1a2bc5130abf382607f
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-07-30 17:24:21 +0300

    [Controllers] Adding PlatformsController.cs

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using OriApps.UniCommand.PlatformService.Data;
+using OriApps.UniCommand.PlatformService.Data.DTO;
+using OriApps.UniCommand.PlatformService.Models;
+
+namespace OriApps.UniCommand.PlatformService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformsController(IPlatformRepository platformRepository, IMapper mapper) : ControllerBase
+{
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<PlatformReadDTO>>> GetPlatforms()
+	{
+		Console.WriteLine(nameof(GetPlatforms));
+		
+		var platforms = await platformRepository.GetAllPlatformsAsync();
+		
+		return Ok(mapper.Map<IEnumerable<PlatformReadDTO>>(platforms));
+	}
+	
+	[HttpGet("{id}", Name = nameof(GetPlatformById))]
+	public async Task<ActionResult<PlatformReadDTO>> GetPlatformById(int id)
+	{
+		Console.WriteLine(nameof(GetPlatformById));
+		
+		var platform = await platformRepository.GetPlatformByIdAsync(id);
+
+		if (platform == null)
+		{
+			return NotFound();
+		}
+		
+		return Ok(mapper.Map<PlatformReadDTO>(platform));
+	}
+	
+	[HttpPost]
+	public async Task<ActionResult<PlatformReadDTO>> CreatePlatform([FromBody] PlatformCreateDTO platformDto)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+
+		var platform = mapper.Map<Platform>(platformDto);
+
+		if (platform.Cost == null)
+		{
+			return NotFound($"CostType with ID {platformDto.CostTypeID} not found.");
+		}
+
+		await platformRepository.AddPlatformAsync(platform);
+		await platformRepository.SaveChangesAsync();
+
+		var readDto = mapper.Map<PlatformReadDTO>(platform);
+
+		return CreatedAtAction(nameof(GetPlatformById), new { id = platform.Id }, readDto);
+	}
+
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,9 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<PlatformServiceDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<PlatformServiceDbContext>(options => options.UseInMemoryDatabase("InMemoryPlatformServiceDb"));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.UseRouting();
+app.MapControllers();
 
 await DatabaseSeeder.SeedAsync(app);
 
