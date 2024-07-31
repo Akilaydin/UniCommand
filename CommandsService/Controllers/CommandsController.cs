da73e6cea9b6d116d1520cda4d37542328ca6dcd
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-07-31 13:31:22 +0300

    [Commands] Adding Commands service

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OriApps.UniCommand.CommandsService.Controllers;
+
+public class CommandsController(HttpClient httpClient) : ControllerBase
+{
+	
+}
---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OriApps.UniCommand.CommandsService.Controllers;
+
+[Route("api/commands/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+	[HttpPost]
+	public ActionResult TestInboundConnection()
+	{
+		Console.WriteLine("TestInboundConnection");
+		
+		return Ok("InboundConnection successful");
+	}
+}
