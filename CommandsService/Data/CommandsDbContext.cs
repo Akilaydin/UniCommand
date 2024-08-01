2ad3c63830d91f9800956edb3c3b9819389fee93
Artem Ovchinnikov <akilaydin2@gmail.com>
2024-08-01 21:30:14 +0300

    [Commands] Adding IRepository and models

---
+++ CommandsService/Data/CommandsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using OriApps.UniCommand.CommandsService.Models;
+
+namespace OriApps.UniCommand.CommandsService.Data;
+
+public class CommandsDbContext : DbContext
+{
+	public DbSet<Platform> Platforms;
+	public DbSet<Command> Commands;
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Platform>().HasMany(p => p.Commands).WithOne(c => c.Platform);
+		
+		modelBuilder.Entity<Command>().HasOne(c => c.Platform).WithMany(p => p.Commands);
+	}
+}
---
+++ CommandsService/Models/Command.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OriApps.UniCommand.CommandsService.Models;
+
+public class Command
+{
+	[Key]
+	[Required]
+	public int Id { get; set; }
+	[Required]
+	public string HowTo { get; set; }
+	[Required]
+	public string CommandLine { get; set; }
+	
+	public Platform Platform { get; set; }
+}
